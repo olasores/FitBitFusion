0b92027ab83c3ea7edc9d468986ff16da70d7789
Ola <oyinkansola_o@yahoo.com>
2024-10-31 22:39:21 -0700

    fixed footer a bit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer className="bg-gray-800 p-4 text-center text-white">
+    <footer className="bg-gradient-to-r from-gray-800 to-teal-500 p-4 text-center text-white">
       <p>© 2024 FITBITFUSION. All rights reserved.</p>
     </footer>
   );
